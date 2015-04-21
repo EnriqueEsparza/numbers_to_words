ab2bcfa769697639b5db3d224393c2693013a350
peter & Enrique <student@epicodus.com>
2015-04-21 14:10:47 -0700

    initial rspec test

---
+++ numbers_to_words/README.txt
@@ -0,0 +1,1 @@
+Converting numerals into written numbers.
---
+++ numbers_to_words/spec/numbers_to_words_spec.rb
@@ -0,0 +1,8 @@
+requires('rspec')
+requires('numbers_to_words')
+
+describe('words#numbers_to_words') do
+  it("writes out number based on input numeral") do
+    expect((25).numbers_to_words()).to(eq("twentyfive"))
+  end
+end
